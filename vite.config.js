3a65b0a781068b433b7cbda61169c7834db0834a
Likhon Chandra Das <axtillar@gmail.com>
2024-10-29 18:34:13 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'framer-motion'],
-          animations: ['react-type-animation']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+export default {
+  base: '/axtillar.github.io/', // Update this line
+  // other Vite settings here
+}
